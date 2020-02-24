e97d88cdeaa23c4e461fdadedbf2909d365579a6
solunasalles <m.a.nagorny@gmail.com>
2020-02-24 22:28:47 +0500

    les08-les11

---
+++ les09/les09_db.sql
@@ -0,0 +1,217 @@
+-- Практическое задание по теме “Транзакции, переменные, представления”
+
+-- 1. В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+
+-- наполнение баз
+CREATE DATABASE shop;
+USE shop;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+CREATE DATABASE sample;
+USE sample;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  birthday_at DATE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Сергей', '1987-12-03'),
+  ('Иван', '1965-10-09'),
+  ('Светлана', '1993-12-22'),
+  ('Анатолий', '1989-02-14'),
+  ('Мария', '1998-11-01'),
+  ('Александр', '1988-08-19');
+
+-- выполнение задачи
+START TRANSACTION;
+INSERT INTO sample.users (name, birthday_at, created_at, updated_at) SELECT name, birthday_at, created_at, updated_at FROM shop.users WHERE id = 1;
+DELETE FROM shop.users WHERE id = 1;
+COMMIT;
+
+-- 2. Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
+
+USE shop;
+-- наполним базу
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+-- выполнение задания
+DROP VIEW IF EXISTS prod;
+CREATE VIEW prod AS 
+SELECT products.name as p_name, catalogs.name as c_name
+FROM products
+LEFT JOIN catalogs
+ON products.catalog_id = catalogs.id;
+
+-- 3. (по желанию) Пусть имеется таблица с календарным полем created_at. В ней размещены разряженые календарные записи за август 2018 года '2018-08-01', '2016-08-04', '2018-08-16' и 2018-08-17. Составьте запрос, который выводит полный список дат за август, выставляя в соседнем поле значение 1, если дата присутствует в исходном таблице и 0, если она отсутствует.
+
+-- 4. (по желанию) Пусть имеется любая таблица с календарным полем created_at. Создайте запрос, который удаляет устаревшие записи из таблицы, оставляя только 5 самых свежих записей.
+
+
+-- Практическое задание по теме “Администрирование MySQL” (эта тема изучается по вашему желанию)
+
+-- 1. Создайте двух пользователей которые имеют доступ к базе данных shop. Первому пользователю shop_read должны быть доступны только запросы на чтение данных, второму пользователю shop — любые операции в пределах базы данных shop.
+
+CREATE USER shop_read IDENTIFIED BY 'password';
+CREATE USER shop IDENTIFIED BY 'password1';
+
+GRANT SELECT ON shop.* TO 'shop_read';
+GRANT ALL ON shop.* TO 'shop';
+
+-- 2. (по желанию) Пусть имеется таблица accounts содержащая три столбца id, name, password, содержащие первичный ключ, имя пользователя и его пароль. Создайте представление username таблицы accounts, предоставляющий доступ к столбца id и name. Создайте пользователя user_read, который бы не имел доступа к таблице accounts, однако, мог бы извлекать записи из представления username.
+
+
+
+-- Практическое задание по теме “Хранимые процедуры и функции, триггеры"
+
+-- 1. Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP FUNCTION IF EXISTS hello;
+
+DELIMITER //
+CREATE FUNCTION hello()
+RETURNS VARCHAR(50) NOT DETERMINISTIC
+READS SQL DATA
+BEGIN
+  CASE
+    WHEN CURRENT_TIME() BETWEEN TIME("06:00:00") AND TIME("12:00:00") THEN 
+      RETURN "Доброе утро";
+    WHEN CURRENT_TIME() BETWEEN TIME("12:00:00") AND TIME("18:00:00") THEN
+      RETURN "Добрый день";
+    WHEN CURRENT_TIME() BETWEEN TIME("18:00:00") AND TIME("23:59:59") THEN
+      RETURN "Добрый вечер";
+    WHEN CURRENT_TIME() BETWEEN TIME("00:00:00") AND TIME("06:00:00") THEN
+      RETURN "Доброй ночи";
+  END CASE;
+END//
+
+DELIMITER ;
+
+-- проверка работы функции
+SELECT hello();
+
+-- 2. В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
+
+USE shop;
+-- создадим таблицу и наполним ее
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+-- выполнение задачи (создаем триггеры)
+-- триггер на вставку
+DELIMITER //
+CREATE TRIGGER check_null_insert_products BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'name and description is NULL';
+  END IF;
+END//
+
+-- проверка вставки
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, NULL, 5060.00, 2);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, 'Процессор для настольных персональных компьютеров', 5060.00, 2);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3', 'Процессор для настольных персональных компьютеров', 5060.00, 2);
+
+DELIMITER ;
+
+
+-- триггер на обновление
+-- при указании у обоих полей NEW dсе варианты обновления срабатывают правильно, например, при обновлении description = null, при этом name уже null, будет выведена ошибка
+DELIMITER //
+CREATE TRIGGER check_null_update_products BEFORE UPDATE ON products
+FOR EACH ROW 
+BEGIN
+  IF (NEW.name IS NULL AND NEW.description IS NULL) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'name and description is NULL';
+  END IF;
+END//
+
+DELIMITER ;
+
+-- 3. (по желанию) Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. Числами Фибоначчи называется последовательность в которой число равно сумме двух предыдущих чисел. Вызов функции FIBONACCI(10) должен возвращать число 55.
+
---
+++ les11/les11_db.sql
@@ -0,0 +1,177 @@
+-- Практическое задание по теме “Оптимизация запросов”
+-- 1. Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, catalogs и products в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа и содержимое поля name.
+
+-- создадим и наполним таблицы
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  desription TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+-- выполнение задачи
+-- создадим таблицу logs
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+	tablename VARCHAR(255),
+	entry_id INT,
+	name VARCHAR (255),
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+) ENGINE=Archive;
+
+-- создадим триггеры на таблицы users, catalogs, products
+DELIMITER //
+CREATE TRIGGER logs_from_users AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (tablename, entry_id, name) VALUES ('users', NEW.id, NEW.name);
+END//
+
+CREATE TRIGGER logs_from_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (tablename, entry_id, name) VALUES ('catalogs', NEW.id, NEW.name);
+END//
+
+CREATE TRIGGER logs_from_products AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (tablename, entry_id, name) VALUES ('products', NEW.id, NEW.name);
+END//
+
+-- 2. (по желанию) Создайте SQL-запрос, который помещает в таблицу users миллион записей.
+
+
+
+-- Практическое задание по теме “NoSQL”
+-- 1. В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
+-- скриншот - 
+-- HSET ips '127.0.0.1' 1
+-- HINCRBY ips '127.0.0.1' 1
+-- HSET ips '127.0.0.2' 1
+-- HINCRBY ips '127.0.0.2' 1
+-- HINCRBY ips '127.0.0.2' 1
+-- HGETALL ips
+-- 1) "127.0.0.1"
+-- 2) "2"
+-- 3) "127.0.0.2"
+-- 4) "3"
+
+-- 2. При помощи базы данных Redis решите задачу поиска имени пользователя по электронному адресу и наоборот, поиск электронного адреса пользователя по его имени.
+-- скриншот - 
+-- HSET users 'sergey@mail.ru' sergey
+-- HSET users 'petr@mail.ru' petr
+-- HGET users 'petr@mail.ru'
+-- "petr"
+-- HGET users 'sergey@mail.ru'
+-- "sergey"
+
+-- HSET emails 'sergey' sergey@mail.ru
+-- HSET emails 'petr' petr@mail.ru
+-- HGET emails 'petr'
+-- "petr@mail.ru"
+-- HGET emails 'sergey'
+-- "sergey@mail.ru"
+
+-- 3. Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
+-- команды работы с MongoDB через консоль
+
+-- use shop
+-- db.createCollection('catalogs')
+-- db.createCollection('products')
+
+-- db.catalogs.insert({name: 'Процессоры'})
+-- db.catalogs.insert({name: 'Материнские платы'})
+-- db.catalogs.insert({name: 'Видеокарты'})
+-- db.catalogs.insert({name: 'Жесткие диски'})
+-- db.catalogs.insert({name: 'Оперативная память'})
+
+-- db.products.insert({
+--		name: 'Intel Core i3-8100', 
+--		description: 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 
+--		price: '7890.00', 
+--		catalog_id: ObjectId("507f1f77bcf86cd799439011")})
+
+-- db.products.insert({
+--		name: 'Intel Core i5-7400', 
+--		description: 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 
+--		price: '12700.00', 
+--		catalog_id: ObjectId("507f1f77bcf86cd799439011")})
+
+-- db.products.insert({
+--		name: 'AMD FX-8320E', 
+--		description: 'Процессор для настольных персональных компьютеров, основанных на платформе AMD', 
+--		price: '4780.00', 
+--		catalog_id: ObjectId("507f1f77bcf86cd799439011")})
+
+-- db.products.insert({
+--		name: 'AMD FX-8320', 
+--		description: 'Процессор для настольных персональных компьютеров, основанных на платформе AMD', 
+--		price: '7120.00', 
+--		catalog_id: ObjectId("507f1f77bcf86cd799439011")})
+
+-- db.products.insert({
+--		name: 'ASUS ROG MAXIMUS X HERO', 
+--		description: 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 
+--		price: '19310.00', 
+--		catalog_id: ObjectId("5349b4ddd2781d08c09890f3")})
+
+-- db.products.insert({
+--		name: 'Gigabyte H310M S2H', 
+--		description: 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 
+--		price: '4790.00', 
+--		catalog_id: ObjectId("5349b4ddd2781d08c09890f3")})
+
+-- db.products.insert({
+--		name: 'MSI B250M GAMING PRO', 
+--		description: 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 
+--		price: '5060.00', 
+--		catalog_id: ObjectId("5349b4ddd2781d08c09890f3")})
